b6555fce0ad01a2a34c3f378e77671b52546ca8d
Twan Kruiswijk <twankrui@gmail.com>
2022-03-16 15:54:29 +0100

    Added meta and localisation tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>useaffiliates.com</title>
+        <meta
+          name="description"
+          content="Monetize your content with affiliate marketing."
+        />
+
+        <meta key="title" property="og:title" content="useaffiliates.com" />
+        <meta
+          key="description"
+          property="og:description"
+          content="Monetize your content with affiliate marketing."
+        />
+        <meta key="url" property="og:url" content="https://useaffiliates.com" />
+        <meta key="type" property="og:type" content="website" />
+        <meta
+          key="image"
+          property="og:image"
+          content="https://useaffiliates.com/img/og.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
