6230f38cfb975d198d7ef748313752281557849a
Twan Kruiswijk <twankrui@gmail.com>
2022-04-07 16:04:10 +0200

    Update font-weight category tags

--- src/components/listing/categoryTags.js
+++ src/components/listing/categoryTags.js
@@ -8,7 +8,7 @@ export default function CategoryTags({ categories }) {
 
   const renderCategories = categories.map((c) => {
     const tagStyles =
-      'mr-2 mb-2 text-xs py-1 px-2.5 rounded font-medium capitalize';
+      'mr-2 mb-2 text-xs py-1 px-2.5 rounded font-normal capitalize';
 
     if (c.name === category) {
       return (
