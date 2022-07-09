c5bd97a4b629170eda0f8195213a3ec839c93091
Twan Kruiswijk <twankrui@gmail.com>
2022-07-09 14:27:45 +0200

    Include shared folder in tailwindConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/shared/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
